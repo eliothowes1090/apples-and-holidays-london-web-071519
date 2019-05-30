d1f502e1dac6ff07b2cdffd56849aa078b121777
Eliot Howes <ejhowes@gmail.com>
2019-05-30 20:57:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_supplies_in_holidays(holiday_hash)
       value.each do |holiday, item|
         new_array = []
         item.each {|item| new_array << item}
-        puts "  #{holiday.split"_".capitalize}: #{new_array.join(", ")}"
+        puts "  #{holiday.split("_").capitalize}: #{new_array.join(", ")}"
       end
     end
   end
